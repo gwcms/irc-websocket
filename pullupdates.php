aea796550550e1f7611028e9ad4b22f8b6cd379a
Vidmantas Norkus <vidmantas.norkus@gw.lt>
2018-04-16 18:27:03 +0300

    pullupdates explain

--- pullupdates.php
+++ pullupdates.php
@@ -3,6 +3,7 @@
 //if you would like get realtime updates email me vidmantas.norkus@gw.lt your http://project.com/pullupdates.php link
 
 //switch to deploy user is needed - sudo is used
+//
 //add to /etc/sudoers line:
 //www-data ALL = (root) NOPASSWD: /usr/bin/php /your/directory/pullupdates.php
 
