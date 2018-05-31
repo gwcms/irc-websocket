27503301c2a8c11b7d330c29227378cc89f3c43a
Vidmantas Norkus <vidmantas.norkus@gw.lt>
2018-05-31 17:24:44 +0300

    error reporting

--- lib/websocket_app.class.php
+++ lib/websocket_app.class.php
@@ -209,7 +209,7 @@ class WebSocket_App extends GW_App_Base
 		\WebSocket\Application\IRCApplication::getInstance()->init();
 
 		$this->registerInnerMethod('versionCheck', '5');
-		$this->registerInnerMethod('versionCheck', '15');
+		$this->registerInnerMethod('testErrorReporting', '15');
 
 		
 		
